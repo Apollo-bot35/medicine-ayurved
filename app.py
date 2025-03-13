0f175d06e372db2bc591698bcbef98f83b6b8543
Shreya Badve <71018084+Apollo-bot35@users.noreply.github.com>
2025-03-14 02:04:15 +0530

    new files

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask, request, jsonify, render_template
+from pymongo import MongoClient
+app = Flask(__name__)
+
+client = MongoClient("mongodb+srv://Ayurpharma:medrecom34@cluster0.j0lio.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+db = client.doctor_db
+doctor_collection = db.doctors
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return render_template('index.html')
+
+
+@app.route('/register')
+def about():
+    return render_template('register.html')
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    if request.method == 'POST':
+        data = {
+            "name": request.form['name'],
+            "email": request.form['email'],
+            "password": request.form['password'],
+            "phone": request.form['phone'],
+            "experience": int(request.form['experience']),
+            "certification_id": request.form['certification_id']
+        }
+
+        # Insert data into the database
+        doctor_collection.insert_one(data)
+
+        return jsonify({"message": "Registration successful!"})
+
+@app.route('/login')
+def about1():
+    return render_template('login.html')
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.form['email']
+    password = request.form['password']
+
+    user = doctor_collection.find_one({"email": email, "password": password})
+
+    if user:
+        session['user'] = user['name']
+        return redirect(url_for('show_data'))
+    else:
+        return "Invalid credentials. Please try again."
+
+@app.route('/show_data')
+def show_data():
+    # Fetch all registered doctor data
+    all_data = list(doctor_collection.find())
+    return render_template('show_data.html', data=all_data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
